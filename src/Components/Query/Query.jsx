8f462b0b5838ce5dc85710696b39ee5857f70bc8
Sabbir Hossen <sabbir.ms2000@gmail.com>
2024-05-13 18:05:35 +0600

    quries section is starting

---
+++ src/Components/Query/Query.jsx
@@ -0,0 +1,13 @@
+
+
+const Query = ({ query }) => {
+  const { brand_name } = query
+  console.log(brand_name);
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default Query;
--- src/Pages/Queries/Queries.jsx
+++ src/Pages/Queries/Queries.jsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from "react";
+import Query from "../../Components/Query/Query";
 
 
 const Queries = () => {
+  const [quries, setQuries] = useState([])
+
+  useEffect(() => {
+    fetch('/api.json')
+      .then(res => res.json())
+      .then(data => setQuries(data))
+  }, [])
   return (
     <div>
-      <h2>hello</h2>
+      <h2>BookHub Quries:</h2>
+      {
+        quries.map(query => <Query
+          key={query.id}
+          query={query}
+        ></Query>)
+      }
     </div>
   );
 };
