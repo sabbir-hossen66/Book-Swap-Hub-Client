db82274931890f8a07adf7fe79bb5708fbc9debe
Sabbir Hossen <sabbir.ms2000@gmail.com>
2024-05-13 11:39:53 +0600

    private route created

---
+++ src/Routes/PrivateRoutes/PrivateRoutes.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { AuthContext } from "../../Providers/AuthProviders";
+import { Navigate, useLocation } from "react-router-dom";
+
+
+const PrivateRoutes = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation()
+
+  if (loading) {
+    return <span className="loading loading-spinner text-center loading-md mx-auto text-success"></span>
+  }
+
+  if (user) {
+    return children
+  }
+
+  return <Navigate state={location.pathname} to='/login'></Navigate>
+
+};
+
+export default PrivateRoutes;
