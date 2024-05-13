94f4b7e22cd8a974adbab82ece5b150a6bc60fd3
Sabbir Hossen <sabbir.ms2000@gmail.com>
2024-05-13 12:54:18 +0600

    react lottie done in registration page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -42,7 +42,7 @@ const Login = () => {
       <div className="container flex items-center justify-center min-h-screen px-6 mx-auto">
         <form onSubmit={handleSignIn} className="w-full max-w-md">
 
-          <h1 className="mt-3 text-2xl font-semibold dark:text-[#3B82F6] capitalize sm:text-3xl ">
+          <h1 className="mt-3 text-2xl font-semibold text-[#3B82F6] capitalize sm:text-3xl ">
             sign In</h1>
 
           <div className="relative flex items-center mt-8">
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,49 +1,55 @@
-
+import Lottie from 'lottie-react';
+import lottiePhoto from '../../assets/photos/banner/Animation - 1715582454787.json'
 
 const Register = () => {
   return (
     <div>
-      <h2>hello register</h2>
+
       <section className="bg-white dark:bg-gray-900">
+
+
         <div className="flex justify-center min-h-screen">
           {/* <div className="hidden bg-cover lg:block lg:w-2/5" style="background-image: url('https://images.unsplash.com/photo-1494621930069-4fd4b2e24a11?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=715&q=80')">
           </div> */}
 
-          <div className="flex items-center w-full max-w-3xl p-8 mx-auto lg:px-12 lg:w-3/5">
+          <div className="flex items-center w-full max-w-7xl p-8 mx-auto lg:px-12 lg:w-3/5">
             <div className="w-full">
-              <h1 className="text-2xl font-semibold tracking-wider text-gray-800 capitalize dark:text-white">
-                Get your free account now.
-              </h1>
+              <div className='flex justify-center items-center'>
 
-              <p className="mt-4 text-gray-800 dark:text-gray-400">
-                Let’s get you all set up so you can verify your personal account and begin setting up your profile.
-              </p>
+                <div>
+                  <Lottie className='w-[320px]' animationData={lottiePhoto} />
+                </div>
+                <h1 className="text-2xl font-semibold tracking-wider text-[#5D9AE5] capitalize">
+                  Get your free account now.
+                </h1>
+              </div>
+
               <form className="grid grid-cols-1 gap-6 mt-8 md:grid-cols-2">
                 <div>
-                  <label className="block mb-2 text-sm text-gray-700 dark:text-gray-200">First Name</label>
+                  <label className="block mb-2 text-sm text-[#5D9AE5]">First Name</label>
                   <input type="text" placeholder="John" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                 </div>
 
                 <div>
-                  <label className="block mb-2 text-sm text-gray-700 dark:text-gray-200">Last name</label>
+                  <label className="block mb-2 text-sm text-[#5D9AE5]">Last name</label>
                   <input type="text" placeholder="Snow" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                 </div>
                 <div>
-                  <label className="block mb-2 text-sm text-gray-700 dark:text-gray-200">Email address</label>
+                  <label className="block mb-2 text-sm text-[#5D9AE5]">Email address</label>
                   <input type="email" placeholder="Enter Your Email" name="email" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                 </div>
 
                 <div>
-                  <label className="block mb-2 text-sm text-gray-700 dark:text-gray-200">Password</label>
+                  <label className="block mb-2 text-sm text-[#5D9AE5]">Password</label>
                   <input type="password" placeholder="Enter your password" name="password" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                 </div>
 
                 <div>
-                  <label className="block mb-2 text-sm text-gray-700 dark:text-gray-200">Photo Url</label>
+                  <label className="block mb-2 text-sm text-[#5D9AE5]">Photo Url</label>
                   <input type="photoUrl" placeholder="submit your photoUrl" name="photoUrl" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                 </div>
                 <div>
-                  <label className="block mb-2 text-sm text-gray-700 dark:text-gray-200">Phone number</label>
+                  <label className="block mb-2 text-sm text-[#5D9AE5]">Phone number</label>
                   <input type="text" placeholder="XXX-XX-XXXX-XXX" className="block w-full px-5 py-3 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-lg dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                 </div>
 
@@ -68,6 +74,7 @@ const Register = () => {
             </div>
           </div>
         </div>
+
       </section>
     </div>
   );
