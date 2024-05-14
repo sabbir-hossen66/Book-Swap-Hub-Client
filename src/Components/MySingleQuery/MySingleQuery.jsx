f2bea15f4b6bcb34c34a125af336ef8420b8a85e
Sabbir Hossen <sabbir.ms2000@gmail.com>
2024-05-15 04:01:10 +0600

    in quries page card added

--- src/Components/MySingleQuery/MySingleQuery.jsx
+++ src/Components/MySingleQuery/MySingleQuery.jsx
@@ -2,30 +2,32 @@
 
 const MySingleQuery = ({ book }) => {
   const { productName, brandProduct, title, boycot, productPhoto, email, userName, image, time } = book;
-  console.log(productName);
+
   return (
     <div>
-      <h2>This is single query:{productName}</h2>
-      <div className="max-w-xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
+
+      <div className="max-w-xl mx-auto bg-white shadow-lg rounded-xl overflow-hidden py-4 px-2">
         <div className="relative">
           <img
             className="w-full h-64 object-cover"
-            src="https://via.placeholder.com/500"
+            src={productPhoto}
             alt="book cover"
           />
           <div className="absolute inset-0 bg-black opacity-25"></div>
           <div className="absolute inset-0 flex items-center justify-center">
-            <p className="text-white font-semibold text-lg">Book Title</p>
+            <p className="text-white font-semibold text-lg ">Book Title</p>
           </div>
         </div>
         <div className="px-6 py-4">
           <div className="flex justify-between items-center">
             <div>
-              <div className="font-bold text-xl mb-2">Book Title</div>
-              <p className="text-gray-700 text-base">
-                Some description about the book goes here. Lorem ipsum dolor
-                sit amet, consectetur adipiscing elit.
-              </p>
+              <div className="font-bold text-xl mb-2 hover:underline cursor-pointer text-[#FB923C]"
+              >{title}</div>
+              <p className="text-gray-700 text-base ">product_name: <span className="font-medium text-[#618CF6]">{productName}</span></p>
+              <p className="text-gray-700 text-base ">product_brand: <span className="font-medium text-[#618CF6]">{brandProduct}</span></p>
+              <p className="text-gray-700 text-base ">boycot: <span className="font-medium text-[#618CF6]">{boycot}</span></p>
+
+
             </div>
             <div className="flex flex-col">
               <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-2">
@@ -40,6 +42,20 @@ const MySingleQuery = ({ book }) => {
             </div>
           </div>
         </div>
+
+        {/* set another  */}
+        <div className="mt-4">
+          <div className="flex items-center">
+            <div className="flex items-center">
+              <img className="object-cover h-10 rounded-full" src={image} alt="Avatar" />
+              <a href="#" className="mx-2 font-semibold text-[#FB923C] " tabIndex="0" role="link">{userName}</a>
+            </div>
+            <span className="mx-1 text-xs font-medium text-[#618CF6]">{time}</span>
+
+          </div>
+          <p className="text-sm font-medium text-gray-700">email: <span className="text-[#618CF6]"> {email}</span></p>
+        </div>
+
       </div>
     </div>
   );
--- src/Pages/MyQuries/MyQuries.jsx
+++ src/Pages/MyQuries/MyQuries.jsx
@@ -40,7 +40,7 @@ const MyQuries = () => {
         />
       </div>
       {/* stylishling react typer finished */}
-      <div>
+      <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 py-16">
 
         {
           books.map(book => <MySingleQuery
